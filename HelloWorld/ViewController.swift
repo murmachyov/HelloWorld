b6fdfbe1782b052aff741d4b6d813e3e677fbb66
Dmitry Murmachyov <murmachyov@gmail.com>
2021-03-24 08:52:36 +0300

    Setup code final

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var helloWorldlable: UILabel!
+    @IBOutlet weak var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldlable.isHidden = true
+        showTextButton.layer.cornerRadius = 15
+        
     }
 
-
+    @IBAction func showTextButtonPresswnd() {
+        helloWorldlable.isHidden.toggle()
+        if helloWorldlable.isHidden {
+            showTextButton.setTitle("Show Twxt", for: .normal)
+        } else {
+            showTextButton.setTitle("Hide Text", for: .normal)
+            
+        }
+    }
+    
 }
 
